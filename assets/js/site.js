38937a5fbe7fb2ddb2466276ca6809a8a8b491cc
Jean Gerard Bousiquot <jgbneatdesign@gmail.com>
2018-01-06 21:35:53 -0500

    work in progress

---
+++ assets/js/site.js
@@ -0,0 +1,7 @@
+// Side Menu
+var sidenav = document.querySelector('.sidenav');
+var instance = M.Sidenav.init(sidenav, {closeOnClick: true});
+
+// Parallax
+var parallax = document.querySelector('.parallax');
+var instance = M.Parallax.init(parallax);
