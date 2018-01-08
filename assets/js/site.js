d6393cada4e405122ce46666d191c21febdd38bc
Jean Gerard Bousiquot <jgbneatdesign@gmail.com>
2018-01-07 19:38:22 -0500

    work in progress: started adding some works

--- assets/js/site.js
+++ assets/js/site.js
@@ -3,5 +3,9 @@ var sidenav = document.querySelector('.sidenav');
 var instance = M.Sidenav.init(sidenav, {closeOnClick: true});
 
 // Parallax
-var parallax = document.querySelector('.parallax');
-var instance = M.Parallax.init(parallax);
+// var parallax = document.querySelector('.parallax');
+// var instance = M.Parallax.init(parallax);
+
+// Modals
+ var modal = document.querySelector('.modal');
+ var instance = M.Modal.init(modal);
